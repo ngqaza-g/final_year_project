a2aaf557fce57db52565daf3af7eed7af83538e4
Ngqabutho Dlamini <ngqaza.dlamz47@gmail.com>
2022-03-19 18:40:11 +0200

    Changed from promises to async/await Added change password functionality

---
+++ backend/authentication_modules/change_password.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+const change_password = async (req, res, next)=>{
+    
+    try{
+        const { current_password, new_password } = req.body;
+        //Validate the token
+    
+        //Get the user
+        const user = req.user;
+        //Compare the passwords
+        
+        const isMatch = await bcrypt.compare(current_password, user.password);
+        if(isMatch){
+             // Update the database with the new password
+            const hashedPassword = await bcrypt.hash(new_password, 10);
+            await User.updateOne({_id: user._id} , {password: hashedPassword});
+          
+            next();
+        }else{
+            res.status(401).json({msg: "Passwords dont match"});
+        }
+
+    }catch{
+            // Handle errors or success
+        res.status(500).json({msg: "Server Error occured"});
+    }
+}
+
+module.exports = change_password;
+
+
+
+
+
--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const change_password = require('../authentication_modules/change_password');
 const login = require('../authentication_modules/login');
 const logout = require('../authentication_modules/logout');
 const register = require('../authentication_modules/register');
@@ -6,12 +7,23 @@ const validate_token = require('../authentication_modules/validate_token');
 
 const router = express.Router();
 
-router.post('/', validate_token);
+router.post('/', validate_token, (req, res)=>{
+    const user = req.user;
+    const userData = {
+        name : user.name,
+        email : user.email,
+        username : user.username,
+        role: user.role
+    };
+    res.json({msg : "logged in", user : userData});
+});
 
-router.post('/register', [register, login]);
+router.post('/register', register);
 
 router.post('/login', login);
 
-router.post('/logout', logout);
+router.post('/logout', [validate_token ,logout]);
+
+router.post('/change_password', [validate_token, change_password, logout]);
 
 module.exports = router;
